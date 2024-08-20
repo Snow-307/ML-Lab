e838b8739c539b4807fc1d610df17f699c2a3fe6
Snow-307 <sneha.thejus@gmail.com>
2024-08-20 18:55:08 +0530

    Lab-3 Updated code with project dataset

--- Lab-3/Lab-3.py
+++ Lab-3/Lab-3.py
@@ -1,136 +1,92 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 13 19:46:29 2024
+Created on Tue Aug 20 11:20:21 2024
 
 @author: sneha_xqbh6g1
 """
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from scipy.spatial.distance import minkowski
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, classification_report, precision_score, recall_score, f1_score
+import matplotlib.pyplot as plt
+
+data = pd.read_csv(r'C:\Amrita\Sem 5\ML\hm\image_data3.csv')
+
+class_ajanta = data[data['label'] == 'Ajanta Caves']
+class_mysore = data[data['label'] == 'mysore_palace']
+class_gateway = data[data['label'] == 'Gateway of India']
 
 
+features_ajanta = class_ajanta.drop('label', axis=1)
+features_mysore = class_mysore.drop('label', axis=1)
+features_gateway = class_gateway.drop('label', axis=1)
 
 
-def class_mean(class1,class2):
+features_ajanta = features_ajanta.to_numpy()
+features_mysore = features_mysore.to_numpy()
+features_gateway = features_gateway.to_numpy()
+
+
+def centroids():
+    centroid_ajanta = np.mean(features_ajanta, axis=0)
+    centroid_mysore = np.mean(features_mysore, axis=0)
+    centroid_gateway = np.mean(features_gateway, axis=0)
+    print("Centroid of Ajanta Caves:", centroid_ajanta)
+    print("Centroid of Mysore Palace:", centroid_mysore)
+    print("Centroid of Gateway of India:", centroid_gateway)
+
+    dist_ajanta_mysore = np.linalg.norm(centroid_ajanta - centroid_mysore)
+    dist_ajanta_gateway = np.linalg.norm(centroid_ajanta - centroid_gateway)
+    dist_mysore_gateway = np.linalg.norm(centroid_mysore - centroid_gateway)
     
-    centroid1 = np.mean(class1, axis=0)
-    centroid2 = np.mean(class2, axis=0)
-    Euclidean_dist = np.linalg.norm(centroid1 - centroid2)
-    
-    print("Centroid of class 1:\n",centroid1)
-    print("\n Centroid of class 2:\n",centroid2)
-    print("\n Euclidean Disctance:",Euclidean_dist)
-
-def spread(class1,class2):
-    spread1 = np.std(class1, axis=0)
-    spread2 = np.std(class2, axis=0)
-    print("Spread of Class 1:\n",spread1)
-    print("Spread of Class 2:\n",spread2)
-
-def histo():
-    feature_data = data['BMI']
-    plt.hist(feature_data, bins=10)  # Adjust bins as needed
-    plt.xlabel('BMI')
-    plt.ylabel('Frequency')
-    plt.show()
-    mean_bmi = np.mean(feature_data)
-    variance_bmi = np.var(feature_data)
-    
-    print("Mean of BMI:",mean_bmi)
-    print("Variance of BMI:",variance_bmi)
+    print("Euclidean Distance (Ajanta Caves & Mysore Palace):", dist_ajanta_mysore)
+    print("Euclidean Distance (Ajanta Caves & Gateway of India):", dist_ajanta_gateway)
+    print("Euclidean Distance (Mysore Palace & Gateway of India):", dist_mysore_gateway)
 
 
-def minkowski_q():
-    required = ['Age', 'BMI', 'Sleep Duration (Hours)']
-    vector1 = data.loc[0, required].values
-    vector2 = data.loc[1, required].values
-    
-    vector1 = np.array(vector1, dtype=float)
-    vector2 = np.array(vector2, dtype=float)
-    
-    distances = [minkowski(vector1, vector2, p=r) for r in range(1, 11)]
-    
-    plt.plot(range(1, 11), distances)
-    plt.xlabel('r')
-    plt.ylabel('Minkowski Distance')
-    plt.show()
+def spread():
+    spread_ajanta = np.std(features_ajanta, axis=0)
+    spread_mysore = np.std(features_mysore, axis=0)
+    spread_gateway = np.std(features_gateway, axis=0)
+    print("Spread of Ajanta Caves:", spread_ajanta)
+    print("Spread of Mysore Palace:", spread_mysore)
+    print("Spread of Gateway of India:", spread_gateway)
 
 def train_test():
-    
-    X = data[['Age', 'Gender', 'Race', 'BMI', 'Smoking', 'Heavy Drinking', 'Sleep Duration (Hours)', 'Arthritis', 'Liver Condition', 'Parental Osteoporosis']]
-    y = data['Osteoporosis']
-
+    X = data.drop('label', axis=1)
+    y = data['label']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-    return(X_train, X_test, y_train, y_test)
+    return X_train, X_test, y_train, y_test
 
-def knn(X_train,y_train):
+
+def knn(X_train, y_train):
     neigh = KNeighborsClassifier(n_neighbors=3)
     neigh.fit(X_train, y_train)
     return neigh
-    
-def check_accu(neigh):
+
+
+def evaluate_performance(neigh, X_test, y_test):
     accuracy = neigh.score(X_test, y_test)
     print(f'Accuracy: {accuracy}')
-    predictions = neigh.predict(X_test)
-    print(f"Predictions: {predictions}")
-    specific_prediction = neigh.predict([X_test.iloc[0]])
-    print(f"Prediction for first test vector: {specific_prediction}")
-    accuracies = []
-    k_values = range(1, 12)
-
-    for k in k_values:
-        knn = KNeighborsClassifier(n_neighbors=k)
-        knn.fit(X_train, y_train)
-        accuracies.append(knn.score(X_test, y_test))
-        
-        plt.plot(k_values, accuracies, marker='o')
-        plt.title('Accuracy vs. k in kNN')
-        plt.xlabel('k')
-        plt.ylabel('Accuracy')
-        plt.show()
-        
     y_pred = neigh.predict(X_test)
-
-
     cm = confusion_matrix(y_test, y_pred)
     print(f"Confusion Matrix:\n{cm}")
-
-    precision = precision_score(y_test, y_pred, pos_label='Yes')
-    recall = recall_score(y_test, y_pred, pos_label='Yes')
-    f1 = f1_score(y_test, y_pred, pos_label='Yes')
-
+    precision = precision_score(y_test, y_pred, average='macro')
+    recall = recall_score(y_test, y_pred, average='macro')
+    f1 = f1_score(y_test, y_pred, average='macro')
     print(f"Precision: {precision}")
     print(f"Recall: {recall}")
     print(f"F1-Score: {f1}")
-    
-    
-    
-    
-data = pd.read_csv('data.csv')
-class1_data = data[data['Osteoporosis'] == "Yes"]  
-class2_data = data[data['Osteoporosis'] == "No"]  
 
 
-features_class1 = class1_data[['Age', 'Gender', 'Race', 'BMI', 'Smoking', 'Heavy Drinking', 'Sleep Duration (Hours)', 'Arthritis', 'Liver Condition', 'Parental Osteoporosis']]
-features_class2 = class2_data[['Age', 'Gender', 'Race', 'BMI', 'Smoking', 'Heavy Drinking', 'Sleep Duration (Hours)', 'Arthritis', 'Liver Condition', 'Parental Osteoporosis']]
-
-numeric_class1 = class1_data.select_dtypes(include=['number'])
-numeric_class2 = class2_data.select_dtypes(include=['number'])
-
-'''
-class_mean(numeric_class1, numeric_class2)
-spread(numeric_class1, numeric_class2)
-histo()
-minkowski_q()
-'''
+centroids()
+spread()
 
 X_train, X_test, y_train, y_test = train_test()
-neigh=knn(X_train,y_train)
-check_accu(neigh)
+neigh = knn(X_train, y_train)
+evaluate_performance(neigh, X_test, y_test)
+
 
 
